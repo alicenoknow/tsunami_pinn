b13c195c61e4a0b9373455599882cadffd86e453
Alicja Niewiadomska <micek2000@gmail.com>
2024-06-18 13:08:25 +0200

    Colab version

--- run_profile.py
+++ run_profile.py
@@ -12,7 +12,6 @@ from environment.mesh_env import MeshEnvironment
 from environment.simple_env import SimpleEnvironment
 from loss.loss import Loss
 from loss.relo_loss import ReloLoss
-from loss.softadapt_loss import SoftAdaptLoss
 from loss.wave_equations import wave_equation, wave_equation_simplified
 from model.pinn import PINN
 from train.params import LossFunction, SimulationParameters
@@ -144,10 +143,6 @@ def setup_loss(environment,
 
     if params.LOSS == LossFunction.RELO:
         return ReloLoss(environment, initial_condition, wave_equation)
-    elif params.LOSS == LossFunction.SOFTADAPT:
-        return SoftAdaptLoss(environment, initial_condition, wave_equation)
-    elif params.LOSS == LossFunction.MIX:
-        return SoftAdaptLoss(environment, initial_condition, wave_equation)
     return Loss(environment, initial_condition, wave_equation)
 
 
